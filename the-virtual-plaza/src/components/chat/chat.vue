7e6b714239628ca1849799f61eeee9757a28f29e
frazier_g <garyfrazier95@yahoo.com>
2020-06-17 20:36:18 -0400

    discord

--- the-virtual-plaza/src/components/chat/chat.vue
+++ the-virtual-plaza/src/components/chat/chat.vue
@@ -1,12 +1,14 @@
 <template>
     <div id="chatContainer" class="chatContainer">
-        <div id="rt-e08c1cbc5b3d5c1d26b15d59db6d3860"></div> 
+        <!--<div id="rt-e08c1cbc5b3d5c1d26b15d59db6d3860"></div> -->
+        <iframe class="chat-embed" src="https://titanembeds.com/embed/696015287253729372?defaultchannel=696015287887331380&scrollbartheme=light&theme=IceWyvern&userscalable=false" frameborder="0"></iframe>
     </div>
 </template>
 
 <style>
     .chatContainer,
     .chatContainer iframe,
+    .chat-embed,
     .rumbletalk-embed {
         width: 100% !important;
         height: 100% !important;
@@ -30,7 +32,7 @@
 
         },
         ready: function () {
-            if (typeof RumbleTalkChat !== 'undefined') {
+            /*if (typeof RumbleTalkChat !== 'undefined') {
                 RumbleTalkChat.YrR0KjvL = undefined
             }
 
@@ -38,7 +40,7 @@
             chatScript.setAttribute('async', 'true')
             chatScript.setAttribute('src', 'https://rumbletalk.com/client/?YrR0KjvL')
          
-            document.getElementById("chatContainer").appendChild(chatScript)
+            document.getElementById("chatContainer").appendChild(chatScript)*/
 
         }
     }
