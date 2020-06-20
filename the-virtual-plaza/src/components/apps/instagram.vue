c2c16f7b85ae159eadb7f601ba4da480b1512a74
frazier_g <garyfrazier95@yahoo.com>
2020-06-20 17:40:58 -0400

    gsuite

--- the-virtual-plaza/src/components/apps/instagram.vue
+++ the-virtual-plaza/src/components/apps/instagram.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="instagramContainer">
-        <iframe src="//lightwidget.com/widgets/ab6d5046b80b5d9a9dfbe37af01291d2.html" scrolling="no" allowtransparency="true" class="lightwidget-widget instagramIframe" style="width:100%;border:0;overflow:hidden;"></iframe>
+            <!-- LightWidget WIDGET --><script src="https://cdn.lightwidget.com/widgets/lightwidget.js"></script><iframe src="https://cdn.lightwidget.com/widgets/ab6d5046b80b5d9a9dfbe37af01291d2.html" scrolling="yes" allowtransparency="true" class="lightwidget-widget instagramIframe" style="width:100%;border:0"></iframe>
     </div>
 </template>
 
@@ -8,7 +8,6 @@
     .instagramContainer {
         width: 100%;
         height: 100%;
-        overflow-y: scroll;
     }
 
     .instagramIframe {
